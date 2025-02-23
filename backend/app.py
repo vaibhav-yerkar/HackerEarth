b42e4348540585175986798519f06cd200da8922
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2025-02-23 16:56:31 +0530

    modified port

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, HTTPException
-import uvicorn
-from modules.database import database  as Database
-from modules.model import Student, Grade, Attendance,chatbot_response,Event
-from modules.chatbot import ParentalMonitoringSystem
-from modules.audio_notes import AudioNotes
-from fastapi.responses import FileResponse
-
-from dotenv import load_dotenv
 import os
 
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+
+from modules.audio_notes import AudioNotes
+from modules.chatbot import ParentalMonitoringSystem
+from modules.database import database as Database
+from modules.model import Attendance, Event, Grade, Student, chatbot_response
 
 app = FastAPI(title="Student Management API")
 chatbot = ParentalMonitoringSystem()
@@ -20,19 +20,25 @@ audio_notes = AudioNotes()
 def home():
     return {"message": "Welcome to Student Management System"}
 
+
 @app.post("/add_student")
 def add_student(student: Student):
     db.add_student(student.dict())
     return {"message": "Student added successfully"}
+
 
 @app.post("/add_score")
 def add_score(grade: Grade):
     db.add_score(grade.dict())
     return {"message": "Score added successfully"}
 
+
 @app.post("/add_attendance")
 def add_attendance(attendance: Attendance):
-    return db.add_attendance(attendance.student_id, attendance.attendance_date, attendance.attendance_status)
+    return db.add_attendance(
+        attendance.student_id, attendance.attendance_date, attendance.attendance_status
+    )
+
 
 @app.get("/get_students_profile")
 def get_students_profile(guardian_mob: int, dob: str):
@@ -41,55 +47,68 @@ def get_students_profile(guardian_mob: int, dob: str):
         raise HTTPException(status_code=404, detail=student["error"])
     return student
 
+
 @app.get("/get_all_students")
 def get_all_students():
     return db.get_all_students()
+
 
 @app.get("/get_student_score/{student_id}")
 def get_student_score(student_id: int):
     return db.get_student_score(student_id)
 
+
 @app.get("/get_student_attendance/{student_id}")
 def get_student_attendance(student_id: int):
     return db.get_student_attendance(student_id)
+
 
 @app.delete("/delete_student/{student_id}")
 def delete_student(student_id: int):
     return db.delete_student(student_id)
 
+
 @app.put("/modify_student/{student_id}")
 def modify_student(student_id: int, student: Student):
     return db.modify_student(student_id, student.dict())
+
 
 @app.put("/modify_remark/{student_id}")
 def modify_remark(student_id: int, remark: str):
     return db.modify_remark(student_id, remark)
 
+
 @app.post("/add_event")
 def add_event(event: Event):
     return db.add_event(event.dict())
+
 
 @app.get("/get_events")
 def get_events():
     return db.get_events()
 
+
 @app.delete("/delete_event/{event_id}")
 def delete_event(event_id: int):
     return db.delete_event(event_id)
+
 
 @app.put("/modify_event/{event_id}")
 def modify_event(event_id: int, event: Event):
     return db.modify_event(event_id, event.dict())
 
+
 @app.post("/chatbot")
 def chatbot_response(chatbot_response: chatbot_response):
-    response = chatbot.generate_response(chatbot_response.question, chatbot_response.student_id)
+    response = chatbot.generate_response(
+        chatbot_response.question, chatbot_response.student_id
+    )
     return {"response": response}
 
 
 @app.post("/generate_audio")
 def generate_audio(text: str, lang: str = "en"):
-    
+
     audio = audio_notes.text_to_audio(text, lang)
     if not audio:
         raise HTTPException(status_code=500, detail="Audio generation failed")
@@ -103,4 +122,4 @@ def generate_audio(text: str, lang: str = "en"):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
